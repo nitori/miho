904c58a285a8a3f0e9250e223609bdd071061588
Lars Peter Søndergaard <nitori@chireiden.net>
2018-02-24 13:23:14 +0100

    Add missing files and directories

---
+++ config-example.py
@@ -0,0 +1,11 @@
+# rename to config.py
+
+# should be relative to this files directory
+# if unsure use absolute path
+MIHO_IMAGES_DIR = 'images'
+
+MIHO_WEB_USER = 'webadmin'
+MIHO_WEB_PASS = 'webpassword'
+
+MIHO_API_USER = 'apiadmin'
+MIHO_API_PASS = 'apipassword'

2014a303dd1714af12de332d4107580a1d068dbd
Lars Peter Søndergaard <nitori@chireiden.net>
2018-02-24 10:59:26 +0100

    Add reset function

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,31 @@
 
 import pathlib
+import shutil
 
-from miho import create_app
+import click
+
+from miho import create_app, utils
 
 config_file = pathlib.Path('.').absolute() / 'config.py'
-application = create_app(config_file)
+app = create_app(config_file)
+
+
+@app.cli.command()
+def reset():
+    images_dir = utils.get_images_dir()
+    click.confirm(f'This will delete all files in {str(images_dir)!r}. Continue?', abort=True, default=False)
+    if images_dir.exists() and not images_dir.is_dir():
+        click.echo(f'{str(images_dir)!r} is not a directory.', err=True)
+        return
+
+    if images_dir.exists():
+        shutil.rmtree(images_dir)
+
+    images_dir.mkdir(0o755, exist_ok=True)
+    pathlib.Path(images_dir, 'thumbs').mkdir(0o755, exist_ok=True)
+    pathlib.Path(images_dir, '.keep').touch(0o644, exist_ok=True)
+    pathlib.Path(images_dir, 'thumbs', '.keep').touch(0o644, exist_ok=True)
 
 
 if __name__ == '__main__':
-    application.run()
+    app.run()

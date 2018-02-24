fdf568ab83606fd7db9baade083833156775c6f3
Lars Peter Søndergaard <nitori@chireiden.net>
2018-02-24 10:59:08 +0100

    Add template

--- miho/views/web.py
+++ miho/views/web.py
@@ -1,6 +1,5 @@
 
-
-from flask import Blueprint, url_for
+from flask import Blueprint, render_template
 
 from .. import utils
 
@@ -9,14 +8,6 @@ web = Blueprint('web', __name__)
 
 @web.route('/')
 def index():
-
     images = list(utils.get_images())
     images.sort()
-
-    s = ''
-    for im in images:
-        turl = url_for('static', filename=f'thumbs/{im.thumbnail}')
-        iurl = url_for('static', filename=im.fullname)
-        s += f'<a href="{iurl}"><img src="{turl}"></a> '
-
-    return s
+    return render_template('index.html', images=images)
